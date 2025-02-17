c1664fcb7ced815bcf8ef36db04e7d1302af2071
Marcos Vinicius <avantiermv@gmail.com>
2025-02-17 16:06:45 -0300

    .

---
+++ script/script.js
@@ -0,0 +1,26 @@
+function load() {
+    fetch('./script/data.json')
+        .then(response => response.json())
+        .then(desserts => {
+            const principal = document.querySelector('#desserts-container');
+
+            desserts.map(dessert => {
+                const card = document.createElement("div");
+                card.classList.add("desserts-cards");
+
+                const image = document.createElement("img");
+                image.src = dessert.image.mobile;
+                image.alt = dessert.name;
+
+                const title = document.createElement("h1");
+                title.textContent = dessert.name;
+
+                
+
+                card.appendChild(image);
+                card.appendChild(title);
+                principal.appendChild(card);
+            })
+        });
+}
+load();
