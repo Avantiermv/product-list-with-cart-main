169207c89b165f6a7e487eedafc5a5e39d22d54d
Marcos Vinicius <avantiermv@gmail.com>
2025-03-19 19:22:42 -0300

    🧪Applying changes
    Here I've made a function that calculates some json values, but it's just a test.

--- test/index.js
+++ test/index.js
@@ -2,22 +2,26 @@
 const meusItens = [
     {
         name: "Baldurs gate",
-        price: 1
+        price: 3
     },
     {
         name: "Residencia Maldita 4",
-        price: 1
+        price: 4
     },
+    {
+        name: "IFMA",
+        price: 2.50
+    }
 ]
 
 
+let totalPreço = meusItens.reduce(somarTodosOsItensDoArray, 0);
 
-function somarTodosOsItensDoArray(preçoDoProduto){
-    let totalPreço = preçoDoProduto.price + preçoDoProduto.price;
-    console.log(totalPreço);
+function somarTodosOsItensDoArray(total, item){
+    return total + item.price; 
 }
 
-somarTodosOsItensDoArray(meusItens);
+console.log(totalPreço);
 
 
 
