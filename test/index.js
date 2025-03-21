dc77bb36066c2dd8a9ecbf075aaf4ec3503b63cc
Marcos Vinicius <avantiermv@gmail.com>
2025-03-21 16:07:54 -0300

    ⚙️I did some tests

--- test/index.js
+++ test/index.js
@@ -1,27 +1,19 @@
+function load(){
 
-const meusItens = [
-    {
-        name: "Baldurs gate",
-        price: 3
-    },
-    {
-        name: "Residencia Maldita 4",
-        price: 4
-    },
-    {
-        name: "IFMA",
-        price: 2.50
-    }
-]
+    fetch('./script/data.json').then(response => response.json()).then(desserts => {
 
+        const preçoDosMeusProdutos = []
+        // Adicionar os preços ao array
+         desserts.forEach(dessert => {
+            preçoDosMeusProdutos.push(dessert.price);
+        });
 
-let totalPreço = meusItens.reduce(somarTodosOsItensDoArray, 0);
-
-function somarTodosOsItensDoArray(total, item){
-    return total + item.price; 
+        // Somar os preços
+        const somaTotal = preçoDosMeusProdutos.reduce((acumulador, valorAtual) => acumulador + valorAtual, 0);
+         console.log(somaTotal); // Exibe a soma de todos os preços
+    })
 }
-
-console.log(totalPreço);
+load();
 
 
 
