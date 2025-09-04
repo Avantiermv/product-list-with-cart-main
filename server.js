d66ea0672dddd68dbd14fef4bc8e8943f2fee1f9
Marcos Vinicius <avantiermv@gmail.com>
2025-09-04 20:33:39 -0300

    :wrench: configuring the server to access static files

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 import express from 'express';
 import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 import { router } from './src/routes/routes.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export const app = express();
 
+
+app.get('/api/data/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'src', 'data', 'data.json'));
+});
 app.use(express.urlencoded({extended: true}));
 app.use(express.static('./public'));
 app.use('/', router);
