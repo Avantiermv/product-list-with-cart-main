aa0ad48af00fb2e354f19d2d1615fb94da3a3723
Marcos Vinicius <avantiermv@gmail.com>
2025-09-11 14:48:22 -0300

    :wrench: configuring json data change

--- src/scripts/generateJsonWithId.js
+++ src/scripts/generateJsonWithId.js
@@ -4,10 +4,20 @@ import path from 'path';
 const rawData = fs.readFileSync('./src/data/data.json', 'utf-8');
 const produtos = JSON.parse(rawData);
 
-const produtosComId = produtos.map((item, index) => ({
-  id: index + 1,
-  ...item
-}));
+const produtosComId = produtos.map((item, index) => {
+  const nwimage = {
+    thumbnail: item.image.thumbnail.replace('.assets/', ''),
+    mobile: item.image.mobile.replace('./assets', ''),
+    tablet: item.image.tablet.replace('./assets', ''),
+    desktop: item.image.desktop.replace('./assets', '')
+  };
+
+  return{
+    id: index + 1,
+    ...item,
+    image: nwimage
+  };
+});
 
 fs.writeFileSync('./src/data/data.json', JSON.stringify(produtosComId, null, 2));
 console.log('Arquivo atualizado com IDs!');
