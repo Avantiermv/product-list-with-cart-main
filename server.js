88471c2c206ca1ffd2a23a4324d4472ebf29a7a2
Marcos Vinicius <avantiermv@gmail.com>
2025-10-17 14:23:44 -0300

    :wrench: changing settings

--- server.js
+++ server.js
@@ -3,16 +3,29 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 import { router } from './src/routes/routes.js';
+import { pushDessertTotal, removeDessertTotal, getAllDesserts } from './src/data/allproductsstock.js';
+import { totalmem } from 'os';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 export const app = express();
 
-
+app.use(express.json());
 app.get('/api/data/', (req, res) => {
     res.sendFile(path.join(__dirname, 'src', 'data', 'data.json'));
 });
+app.post('/api/add-dessert', (req, res) => {
+    const dessert = req.body;
+    pushDessertTotal(dessert);
+    res.status(200).json({totalItems: getAllDesserts().length});
+});
+app.delete('/api/remove-dessert', (req, res) => {
+    const dessert = req.body;
+    removeDessertTotal(dessert);
+    res.status(200).json({totalItems: getAllDesserts().length});
+});
+
 app.use(express.urlencoded({extended: true}));
 app.use(express.static('./public'));
 app.use('/', router);
