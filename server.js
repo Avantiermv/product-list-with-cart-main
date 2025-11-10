1c69ebd37dd1982d23ae7ea259563f9ea3aea6d2
Marcos Vinicius <avantiermv@gmail.com>
2025-11-10 16:18:56 -0300

    :wrench: More configs

--- server.js
+++ server.js
@@ -2,9 +2,7 @@ import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
-import { router } from './src/routes/routes.js';
-import { pushDessertTotal, removeDessertTotal, getDessertCount, getSingleItemId } from './src/data/allproductsstock.js';
-import { totalmem } from 'os';
+import { pushDessertTotal, removeDessertTotal, getDessertCount, getSingleItemId, getAddToTotalPrice, getRemoveToTotalPrice, getDessertName, getDessertPrice, getDessertPriceRemovedAdded } from './src/data/allproductsstock.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -12,6 +10,7 @@ const __dirname = dirname(__filename);
 export const app = express(); 
 
 app.use(express.json());
+
 app.get('/api/data/', (req, res) => {
     res.sendFile(path.join(__dirname, 'src', 'data', 'data.json'));
 });
@@ -20,21 +19,29 @@ app.post('/api/add-dessert', (req, res) => {
     pushDessertTotal(dessert);
     res.status(200).json({
         totalItems: getDessertCount(),
-        singleItem: getSingleItemId(dessert.id)
+        singleItem: getSingleItemId(dessert.id),
+        totalPrice: getAddToTotalPrice(),
+        dessertName: getDessertName(dessert.name),
+        dessertPrice: getDessertPrice(dessert.price),
+        dessertPriceTotalAdded: getDessertPriceRemovedAdded(dessert.price)
     });
 });
 app.delete('/api/remove-dessert', (req, res) => {
     const dessert = req.body;
     removeDessertTotal(dessert);
+    const updatedPrice = getRemoveToTotalPrice();
+    const updatedSingleDessertPrice = getDessertPriceRemovedAdded(dessert.price);
     res.status(200).json({
         totalItems: getDessertCount(),
-        singleItem: getSingleItemId(dessert.id)
+        singleItem: getSingleItemId(dessert.id),
+        dessertName: getDessertName(dessert.name),
+        totalPrice: updatedPrice,
+        dessertPriceTotalRemoved: updatedSingleDessertPrice
     });
 });
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.static('./public'));
-app.use('/', router);
 
 app.listen(3000, () => {
     console.log("App rodando na porta 3000!");
