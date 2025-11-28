b82476810762e929f2e6baaedb3a06e4e140fb54
Marcos Vinicius <avantiermv@gmail.com>
2025-11-28 09:14:05 -0300

    :wrench: cart actions and defined arrays

--- src/data/allproductsstock.js
+++ src/data/allproductsstock.js
@@ -1,7 +1,5 @@
-const dataPath = './src/data/data.json';
-
-let dessertAddedArray = [];
-let totalPrice = 0;
+export let dessertAddedArray = [];
+export let totalPrice = 0;
 
 export function pushDessertTotal(dessert){
     dessertAddedArray.push(dessert);
@@ -26,15 +24,11 @@ export function getSingleItemId(id){
 
 export function getAddToTotalPrice(){
     totalPrice = dessertAddedArray.reduce((sum, item) => sum + item.price, 0);
-    console.log(`Items in the cart: `, dessertAddedArray.length);
-    console.log(`Total price: `, totalPrice);
     return totalPrice;
 }
 
 export function getRemoveToTotalPrice(){
     totalPrice = dessertAddedArray.reduce((sum, item) => sum + item.price, 0);
-    console.log(`Items in the cart: `, dessertAddedArray.length);
-    console.log(`Total price: `, totalPrice);
     return totalPrice;
 }
 
@@ -54,3 +48,28 @@ export function getDessertCount() {
     return dessertAddedArray.length;
 }
 
+export function getDessertCountInTheArray(array){
+    return array.reduce((acc, el) => {
+        acc[el.id] = (acc[el.id] || 0) + 1;
+        return acc;
+    }, {});
+}
+
+export function getDessertPriceInTheArray(array){
+    return array.reduce((acc, el) => {
+        acc[el.id] = el.price;
+        return acc;
+    }, {});
+}
+
+export function getDessertSingleTotalPrice(array){
+    return array.reduce((acc, el) => {
+        acc[el.id] = (acc[el.id] || 0) + el.price;
+        return acc;
+    }, {});
+}
+
+export function clearCart(){
+    dessertAddedArray.length = 0;
+    totalPrice = 0;
+}
